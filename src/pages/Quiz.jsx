b9d2cba3de0841a70dea211370469f383806ca4c
saikat samanta <saikatsamanta052@gmail.com>
2023-10-12 10:27:10 +0530

    final design

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -6,7 +6,7 @@ export default function Quiz() {
         const [counter,setCounter] =useState(0);
         const { quizId } = useParams();
         const [show,setShow] = useState(false);
-        const [correct,setCorrect] =useState(false);
+        const [correct,setCorrect] =useState('');
         useEffect(() => {
                 const data = questionSet.filter(v => {
                         if (v.id === quizId)
@@ -23,8 +23,9 @@ export default function Quiz() {
         }
         const ans = (v) =>{
                 setShow(true)
+                
                 if(v === course[0]?.quizQuestion[counter]?.correct[0]){
-                    setCorrect(v)   
+                        setCorrect(v) 
                 }
                 
         }
@@ -32,17 +33,17 @@ console.log(course[0]?.quizQuestion[0]?.question);
 
         return (
                 <div className='h-screen w-screen flex items-center justify-center'>
-                        <div className='h-[90%] w-[90%] bg-white rounded-xl p-20 flex flex-col justify-between'>
-                                <h1 className='text-5xl text-center'> Question: { course[0]?.quizQuestion[counter]?.question} </h1>
+                        <div className='relative h-[90%] w-[90%] bg-white rounded-xl md:p-20 p-8 flex flex-col  justify-between'>
+                                <h1 className='md:text-5xl text-xl text-center'> Question: { course[0]?.quizQuestion[counter]?.question} </h1>
                                 
-                                <div className='flex justify-center'>
+                                <div className='flex justify-center md:flex-row flex-col'>
                                         {
                                                 course[0]?.quizQuestion[counter]?.answer.map(v=>{
-                                                        return <div  className={` hover:animate-animation-thump h-44 w-80 bg-cyan-300 m-6 rounded-2xl flex items-center justify-center text-3xl cursor-pointer`} onClick={()=>{ans(v)}} > {v} </div>
+                                                        return <div  className={` ${show&&v==course[0]?.quizQuestion[counter]?.correct[0]?'bg-green-300': show? 'bg-red-300':'bg-cyan-300' } hover:animate-animation-thump md:h-44 md:w-80 h-14  bg-cyan-300 m-6 rounded-2xl flex text-center items-center justify-center text-3xl cursor-pointer`} onClick={()=>{ans(v)}} > {v} </div>
                                                 })
                                         }
                                 </div>
-                                { show && <button className='text-3xl rounded-lg bg-cyan-300 h-16 w-32 ml-auto'onClick={nextQues}  >Next</button>  } 
+                                { show && <button className='absolute bottom-5 right-5 text-3xl rounded-lg bg-cyan-300 h-16 w-32 ml-auto'onClick={nextQues}  >Next</button>  } 
                                
                         </div>
                 </div>
